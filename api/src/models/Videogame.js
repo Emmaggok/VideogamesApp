21b9fc938b9b50cd1d12c76d657c11cb8034e840
Emmaggok <emmanuelblackgg@gmail.com>
2021-10-25 22:52:32 -0300

    Defining_Models

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUID } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -8,5 +8,24 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id : {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    launch: {
+      type: DataTypes.STRING
+    },
+    rating: {
+      type: DataTypes.FLOAT
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    }
   });
 };
